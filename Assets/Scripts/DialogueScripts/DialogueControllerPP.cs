7cc8dd43448ea1d3d3ed3fb889985167ba76188d
Alex Chheng <alexchheng1998@gmail.com>
2025-06-22 20:45:27 -0700

    Reapply "Merge branch 'main' into KingdomCross-dev"
    This reverts commit 8cf11f7e715cae011f24d297a8211cc6a79630d9.

--- Assets/Scripts/DialogueScripts/DialogueControllerPP.cs
+++ Assets/Scripts/DialogueScripts/DialogueControllerPP.cs
@@ -59,6 +59,7 @@ public class DialogueControllerPP : MonoBehaviour
             }
             else if (conEnded && !isTyping)
             {
+                
                 EventBusPP<ConversationEndEvent>.Raise(new ConversationEndEvent());
                 EndConvo();
                 return;
@@ -66,13 +67,12 @@ public class DialogueControllerPP : MonoBehaviour
             }
         }
 
-        if (pharagraphs.Count == 0 && pharagraphEnded && dialogueInstances.Count != 0 && !isTyping)
+        if (pharagraphEnded && dialogueInstances.Count != 0 && !isTyping)
         {
             curInstance = dialogueInstances.Dequeue();
             speakerNameTextBox.text = curInstance.speakerName;
             pharagraphEnded = false;
             
-
         }
         DisplayNextPharagraph();
 
@@ -93,7 +93,7 @@ public class DialogueControllerPP : MonoBehaviour
         {
             if (!pharagraphEnded)
             {
-                StartPharagraph();
+                StartParagraph();
             }
             //Make sure to not stop if it is still typing 
             else if (pharagraphEnded && !isTyping)
@@ -102,7 +102,7 @@ public class DialogueControllerPP : MonoBehaviour
             }
         }
 
-        if (!isTyping)
+        if (!isTyping )
         {
             curPharagraph = pharagraphs.Dequeue();
             typeDialogueCoroutine = StartCoroutine(TypeDialogueText(curPharagraph));
@@ -139,12 +139,14 @@ public class DialogueControllerPP : MonoBehaviour
     }
     private void EndConvo()
     {
+        pharagraphEnded = false;
         conEnded = false;
         gameObject.SetActive(false);
     }
 
-    private void StartPharagraph()
+    private void StartParagraph()
     {
+        print("Loading in new paragraphs");
         //Queue them pharagraphs 
         for (int i = 0; i < curInstance.pharagraphs.Length; i++)
         {
