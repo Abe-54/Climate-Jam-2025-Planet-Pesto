43d6794836e58e7d8939afa6acd777051037682f
KingdomCross <firefox2025@duck.com>
2025-07-09 15:50:47 -0700

    OnTriggerEnter2D Spawn FireWall

--- Assets/Scripts/Triggers/FireWallTrigger/FireWallTriggerPP.cs
+++ Assets/Scripts/Triggers/FireWallTrigger/FireWallTriggerPP.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class FireWallTriggerPP : MonoBehaviour
 {
+    [SerializeField] private Transform spawnPoint; // Reference to the spawn point for the firewall, can be set in the inspector or dynamically assigned
     [SerializeField] private GameObject fireWallPrefab; // Reference to the firewall prefab, can be set in the inspector or dynamically assigned
-    [SerializeField] private float spawnTimer = 1f; // Duration for which the firewall will be spawned between spawns
-    [SerializeField] private int maxSpawns = 8; // Maximum number of times the firewall can be spawned 
     private bool isFireWallTriggered = false;
-    private float timer = 0f; // Store the initial spawn timer value
     EventBindingPP<ConversationEndEvent> conversationEndEvent;
     EventBindingPP<ConversationStartEvent> conversationStartEvent;
 
@@ -27,10 +25,7 @@ public class FireWallTriggerPP : MonoBehaviour
 
     public void HandleConversationEndEvent(ConversationEndEvent conversationEndEvent)
     {
-        if (conversationEndEvent.eventName == "FireWallTriggered")
-        {
-            isFireWallTriggered = true;
-        }
+
     }
 
     public void HandleConversationStartEvent(ConversationStartEvent conversationStartEvent)
@@ -38,22 +33,13 @@ public class FireWallTriggerPP : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (isFireWallTriggered == true && maxSpawns > 0 && timer < 0)
+        if (collision.gameObject.CompareTag("Player") && !isFireWallTriggered && GameManagerPP.instance.GetCreatureFlag())
         {
-            timer = spawnTimer; // Reset the timer to the initial value
-            maxSpawns--; // Decrease the number of spawns left
-            Instantiate(fireWallPrefab, new Vector2(transform.position.x, transform.position.y), transform.rotation);
-        }
-        else if (timer >= 0)
-        {
-            timer -= Time.deltaTime; // Decrease the timer by the time passed since last frame
-        }
-        else if (maxSpawns < 0)
-        {
-            isFireWallTriggered = false; // Stop spawning firewalls when max spawns are reached
+            // Trigger the firewall when the player enters the trigger area
+            isFireWallTriggered = true;
+            Instantiate(fireWallPrefab, new Vector2(spawnPoint.position.x, spawnPoint.position.y), spawnPoint.rotation);
         }
     }
 }
